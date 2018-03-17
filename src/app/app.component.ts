9da1f7d93d2a4e586dcbfd5a23becefc4cfc479d
Ritwick Dey <ritwickdey@outlook.com>
2018-03-17 22:40:55 +0530

    fixed animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { sequence, trigger, stagger, animate, style, group, query, transition, keyframes, animateChild } from '@angular/animations';
-// const query = (s, a, o = ) => q(s, a, o);
+import { trigger, transition, style, group, query, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -30,26 +29,9 @@ import { sequence, trigger, stagger, animate, style, group, query, transition, k
   ]
 })
 export class AppComponent {
-  title = 'app';
-  animationType = {
-    value: 'leftToRight',
-    count: 2
-  };
 
-  triggerAnimation() {
-    const type = this.toogleAnimationType().value;
-    console.log(type);
-    return type;
+  triggerAnimation(outlet) {
+    if (outlet.isActivated)
+      return outlet.activatedRoute.component.name;
   }
-
-  // Angular Checkes animation state two times - I don't know why
-  toogleAnimationType() {
-    if (this.animationType.count === 0) {
-      this.animationType.value = this.animationType.value === 'leftToRight' ? 'rightToLeft' : 'leftToRight';
-      this.animationType.count = 2;
-    }
-    this.animationType.count--;
-    return this.animationType;
-  }
-
 }
